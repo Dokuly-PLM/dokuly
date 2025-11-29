18ca69c4c1fab28bc189882fab53a6be63c5b9e0
Erik Buer <buer94@gmail.com>
2025-11-29 08:20:18 +0100

    Remove sales opprotunitites app

--- dokuly/sales_opportunities/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .api import SalesOpportunityViewSet
-
-router = routers.DefaultRouter()
-router.register('api/sales_opportunities',
-                SalesOpportunityViewSet, 'sales_opportunities')
-
-urlpatterns = router.urls
